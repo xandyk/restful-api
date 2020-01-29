2b5534f2336edbe2c1fb96f79a1219153bda546b
Sandy K <sandy@Sandys-MBP.hitronhub.home>
2020-01-29 11:09:59 -0800

    added error handling

--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,26 @@ const router = express.Router();
 const Ninja = require("../models/ninjas");
 
 // get a list of ninjas from the db
-router.get("/ninjas", (req, res) => {
+router.get("/ninjas", (req, res, next) => {
   res.send({ type: "GET" });
 });
 
 // add a new ninja to the db
-router.post("/ninjas", (req, res) => {
-  Ninja.create(req.body).then(function(ninja) {
-    res.send(ninja);
-  });
+router.post("/ninjas", (req, res, next) => {
+  Ninja.create(req.body)
+    .then(function(ninja) {
+      res.send(ninja);
+    })
+    .catch(next);
 });
 
 // update a ninja in the db
-router.put("/ninjas/:id", (req, res) => {
+router.put("/ninjas/:id", (req, res, next) => {
   res.send({ type: "PUT" });
 });
 
 // delete a ninja from the db
-router.delete("/ninjas/:id", (req, res) => {
+router.delete("/ninjas/:id", (req, res, next) => {
   res.send({ type: "DELETE" });
 });
 
--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use("/api", require("./routes/api"));
 
+// error handling middleware
+app.use(function(err, req, res, next) {
+  // console.log(err);
+  res.status(422).send({ error: err.message });
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, () => {
   console.log("Server started on port 4000");
